4e2f811582b96609400a9d940454b5bf46ba4e4a
walt-mutschler <walt.mutschler@secondstreet.com>
2024-12-13 17:24:06 -0600

    Update classic-javascript.js

--- examples/client-side-workflow/classic-javascript.js
+++ examples/client-side-workflow/classic-javascript.js
@@ -4,7 +4,7 @@
  * This implementation assumes a client side workflow.
  */
 function ClassicJavaScriptExample() {
-  this.id = 4;
+  this.id = X;
   this.loginStrategy = 1;
 
   this._loginHandlers = [];
